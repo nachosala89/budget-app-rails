919da67091cd99af76697ba6d10e3cac777555a4
nachosala89 <nachosala89@gmail.com>
2022-02-08 16:39:12 -0300

    Add devise authentication

--- app/controllers/transacs_controller.rb
+++ app/controllers/transacs_controller.rb
@@ -1,4 +1,5 @@
 class TransacsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_transac, only: %i[ show edit update destroy ]
 
   # GET /transacs or /transacs.json
