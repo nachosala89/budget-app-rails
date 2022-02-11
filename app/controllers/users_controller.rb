034dbe277021c9028be6296ed16d8d23d2c01098
nachosala89 <nachosala89@gmail.com>
2022-02-11 11:03:10 -0300

    Fix linters errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user, only: %i[destroy]
 
-  def show
-  end
+  def show; end
 
   def new
     @user = User.new
